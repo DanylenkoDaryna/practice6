1bf662be771fbc626e91b3b6a7bac876c517faf1
d.danylenko <dinkadanko@gmail.com>
2021-05-10 20:52:03 +0300

    new methods with string manipulation

--- src/ua/nure/danylenko/practice6/part8/Part8.java
+++ src/ua/nure/danylenko/practice6/part8/Part8.java
@@ -1,8 +1,8 @@
 package ua.nure.danylenko.practice6.part8;
 
-import java.util.function.IntPredicate;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Part8 {
 
@@ -18,34 +18,88 @@ public class Part8 {
         vowelsConsCounter("there is a quiet Mouse");
         vowelsConsCounter("I am happy");
 
+        vowelsConsCounter2("HellO");
+        vowelsConsCounter2("there is a quiet Mouse");
+        vowelsConsCounter2("I am happy");
+
+        vowelsConsCounter3("HellO");
+        vowelsConsCounter3("there is a quiet Mouse");
+        vowelsConsCounter3("I am happy");
 
     }
 
+
+
     private static void vowelsConsCounter(String input){
 
+        System.out.println("Method 1");
+
         int vowels = 0;
-        int consonants = 0;
+        int consonants;
 
         String vowelStr ="aeiouy";
-        String editedInput=input.trim().toLowerCase();
+        String editedInput=input.toLowerCase().replaceAll(" ","");
 
-        char[]array = editedInput.toCharArray();
+        char[] inputArray = editedInput.toCharArray();
         char[]vowelArray = vowelStr.toCharArray();
 
-        for(char ch:array){
 
-            for(char vow:vowelArray){
+        for(char letter :inputArray){
 
-                if(ch==vow){
+            for(char vowel:vowelArray){
+                if(letter==vowel){
                     vowels+=1;
                 }
             }
-            
+
         }
 
-        consonants=editedInput.length() - vowels;
+        consonants = inputArray.length - vowels;
 
         System.out.printf("in \'%s\' string %s vowels and %s consonants", input, vowels, consonants);
+        System.out.println();
+
+    }
+
+    private static void vowelsConsCounter2(String input){
+
+        System.out.println("Method 2");
+
+        int vowels = 0;
+        int consonants;
+        char[] editedInput=input.toLowerCase().replaceAll(" ","").toCharArray();
+
+        String vowelStr ="aeiouy";
+        for(int i=0; i<editedInput.length; i++){
+            for(int j=0; j<vowelStr.length(); j++){
+
+                if(editedInput[i]==vowelStr.charAt(j)){
+                    vowels+=1;
+                }
+            }
+        }
+
+        consonants = editedInput.length - vowels;
+
+        System.out.printf("in \'%s\' string %s vowels and %s consonants", input, vowels, consonants);
+        System.out.println();
+
+    }
+
+    private static void vowelsConsCounter3(String input){
+
+        System.out.println("Method 3");
+
+        String editedInput=input.toLowerCase().replaceAll(" ","");
+        String vowelStr ="aeiouy";
+
+        List<Integer> letters = editedInput.chars().boxed().collect(Collectors.toList());
+
+        long vChars = letters.stream()
+                .filter(letter -> vowelStr.indexOf(letter)!=-1).count();
+
+
+        System.out.printf("in \'%s\' string %s vowels and %s consonants", input, vChars, letters.size()-vChars);
         System.out.println();
 
     }
